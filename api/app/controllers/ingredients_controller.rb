2f03dd200ff802a85110e1ec6089ec95d0e2fe45
lillchan <californium98@gmail.com>
2014-08-12 20:16:55 -0700

    adding method to create new ingredient record

--- api/app/controllers/ingredients_controller.rb
+++ api/app/controllers/ingredients_controller.rb
@@ -3,6 +3,10 @@ class IngredientsController < ApplicationController
 		render json: Ingredient.all
 	end
 
+	def create
+		@ingredient = Ingredient.new(params[:ingredient])
+	end
+
 	def show
 		render json: Ingredient.find(params[:id])
 	end
