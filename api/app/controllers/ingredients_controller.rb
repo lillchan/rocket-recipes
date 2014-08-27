feb50abf0d94f062d42481e627de7811e560a704
lillchan <californium98@gmail.com>
2014-08-26 20:41:58 -0700

    adding strong params to recipe and ingredient controllers. bypassing authenticity token in both controllers to stop csrf flags. also, checking that ingredients/recipes are saving corretly, otherwise rendering errors (duh).

--- api/app/controllers/ingredients_controller.rb
+++ api/app/controllers/ingredients_controller.rb
@@ -1,13 +1,27 @@
 class IngredientsController < ApplicationController
+	
+	skip_before_filter  :verify_authenticity_token
+
 	def index
 		render json: Ingredient.all
 	end
 
 	def create
-		@ingredient = Ingredient.new(params[:ingredient])
+		@ingredient = Ingredient.new(user_params)
+		if @ingredient.save
+			render :json => @ingredient
+		else
+			render :json => { :errors => @ingredient.errors.full_messages }
+		end
 	end
 
 	def show
 		render json: Ingredient.find(params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:ingredient).permit(:name, :description)
 	end
 end
--- api/app/controllers/recipes_controller.rb
+++ api/app/controllers/recipes_controller.rb
@@ -1,13 +1,28 @@
 class RecipesController < ApplicationController
+
+	skip_before_filter  :verify_authenticity_token
+
 	def index
 		render json: Recipe.all
 	end
 
 	def create
-		@recipe = Recipe.new(params[:recipe])
+		@recipe = Recipe.new(user_params)
+
+		if @recipe.save
+			render :json => @recipe
+		else
+			render :json =>{ :errors => @recipe.errors.full_messages }
+		end
 	end
 
 	def show
 		render json: Recipe.find(params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:recipe).permit(:name, :description)
 	end
 end
